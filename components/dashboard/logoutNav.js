fa8c44f7639efc6440dc7dd70784063ab853cba9
EmmS21 <44911198+EmmS21@users.noreply.github.com>
2024-01-22 21:23:49 -0800

    Update logoutNav.js

--- components/dashboard/logoutNav.js
+++ components/dashboard/logoutNav.js
@@ -13,8 +13,6 @@ const LogoutNavBar = ({ setShowAuthModal }) => {
             <>
               <div className="flex flex-wrap items-center justify-between w-full lg:w-auto">
                 <Link href="/">
-                  <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
-                    <span>
                       <Image
                         src="/img/logo.svg"
                         alt="N"
@@ -22,9 +20,6 @@ const LogoutNavBar = ({ setShowAuthModal }) => {
                         height="32"
                         className="w-8"
                       />
-                    </span>
-                    <span>Nextly</span>
-                  </span>
                 </Link>
 
                 <Disclosure.Button
@@ -54,9 +49,7 @@ const LogoutNavBar = ({ setShowAuthModal }) => {
                   <>
                     <Link href="/" className="w-full px-6 py-2 mt-3 text-center text-white bg-indigo-600 rounded-md lg:ml-5"
                       onClick={()=> setShowAuthModal(true)}
-                    >         
-                        Start Building
-                    </Link>
+                    >Start Building</Link>
                   </>
                 </Disclosure.Panel>
               </div>
@@ -66,9 +59,7 @@ const LogoutNavBar = ({ setShowAuthModal }) => {
 
         <div className="hidden mr-3 space-x-4 lg:flex nav__item">
           <Link href="/" className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5"
-          >
-              Log Out
-          </Link>
+          >Log Out</Link>
           <ThemeChanger />
         </div>
       </nav>
