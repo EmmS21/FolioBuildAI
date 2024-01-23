4fa5b821c16c1e30cc8ccf6d8469e4e179e8711b
EmmS21 <44911198+EmmS21@users.noreply.github.com>
2024-01-22 21:23:19 -0800

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -14,8 +14,6 @@ const Navbar = ({ setShowAuthModal }) => {
             <>
               <div className="flex flex-wrap items-center justify-between w-full lg:w-auto">
                 <Link href="/">
-                  <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
-                    <span>
                       <Image
                         src="/img/logo.svg"
                         alt="N"
@@ -23,9 +21,6 @@ const Navbar = ({ setShowAuthModal }) => {
                         height="32"
                         className="w-8"
                       />
-                    </span>
-                    <span>Nextly</span>
-                  </span>
                 </Link>
 
                 <Disclosure.Button
@@ -55,9 +50,7 @@ const Navbar = ({ setShowAuthModal }) => {
                   <>
                     <Link href="/" className="w-full px-6 py-2 mt-3 text-center text-white bg-indigo-600 rounded-md lg:ml-5"
                       onClick={()=> setShowAuthModal(true)}
-                    >         
-                        Start Building
-                    </Link>
+                    >Start Building</Link>
                   </>
                 </Disclosure.Panel>
               </div>
@@ -68,9 +61,7 @@ const Navbar = ({ setShowAuthModal }) => {
         <div className="hidden mr-3 space-x-4 lg:flex nav__item">
           <Link href="/" className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5"
             onClick={() => setShowAuthModal(true)}
-          >
-              Start Building
-          </Link>
+          >Start Building</Link>
           <ThemeChanger />
         </div>
       </nav>
